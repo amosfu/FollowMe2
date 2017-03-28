f25a6aeec1045f36058e5898c697effc7c093196
Shuai <fs1984@msn.com>
2017-03-28 16:08:15 -0400

    remove useless imports

--- app/src/main/java/com/shuai/followme2/activity/LoginActivity.java
+++ app/src/main/java/com/shuai/followme2/activity/LoginActivity.java
@@ -18,7 +18,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
